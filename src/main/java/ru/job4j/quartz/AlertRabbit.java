2064afaad373483fa2de5c33000e6e3300718728
Nikolai <kbus94@yandex.ru>
2022-06-22 21:33:15 +0300

    1. Quartz [#175122 #315267]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,8 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
@@ -12,14 +14,14 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class)
                     .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(Integer.parseInt(fileRead().getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -34,11 +36,15 @@ public class AlertRabbit {
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            try (BufferedReader in = new BufferedReader(new FileReader("rabbit.properties"))) {
-                in.lines().forEach(System.out::println);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            System.out.println("Rabbit runs here ...");
         }
+    }
+
+    public static Properties fileRead() throws Exception {
+        Properties properties = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        }
+        return properties;
     }
 }
