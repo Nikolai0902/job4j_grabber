33a5aa7d5fdc09ac62d6170af56a04176cd015f2
Nikolai <kbus94@yandex.ru>
2022-06-22 22:31:17 +0300

    1. Quartz [#175122 #315267]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,8 +3,6 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -21,7 +19,8 @@ public class AlertRabbit {
             JobDetail job = newJob(Rabbit.class)
                     .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(fileRead().getProperty("rabbit.interval")))
+                    .withIntervalInSeconds(Integer.parseInt(fileRead()
+                            .getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -35,14 +34,16 @@ public class AlertRabbit {
 
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
         }
     }
 
     public static Properties fileRead() throws Exception {
         Properties properties = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+        try (InputStream in = AlertRabbit.class
+                .getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
             properties.load(in);
         }
         return properties;
