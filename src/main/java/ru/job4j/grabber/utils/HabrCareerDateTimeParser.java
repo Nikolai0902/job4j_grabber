fe8031fa32986f757961bdbbeef6957a0ae67ac5
Nikolai <kbus94@yandex.ru>
2022-06-24 11:15:26 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -3,6 +3,7 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,13 +12,7 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String st) {
-        String[] splitDate = st.split("T");
-        DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate localDate = LocalDate.parse(splitDate[0], formatterDate);
-
-        String[] splitTime = splitDate[1].split("\\+");
-        DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime localTime = LocalTime.parse(splitTime[0], formatterTime);
-        return LocalDateTime.of(localDate, localTime);
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(st);
+        return zonedDateTime.toLocalDateTime();
     }
 }
